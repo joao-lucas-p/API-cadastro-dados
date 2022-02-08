fe0531bfda2bac182b96af805ae4be081c42f075
João Lucas de Pinho <97855044+joao-lucas-p@users.noreply.github.com>
2022-02-08 16:17:38 -0300

    CORS Comment

--- src/main/java/com/joaolucaspinho/apicadastro/configuration/ConfigCors.java
+++ src/main/java/com/joaolucaspinho/apicadastro/configuration/ConfigCors.java
@@ -10,7 +10,7 @@ public class ConfigCors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // Define as permissões de métodos de um ou mais endereços. 
-        registry.addMapping("/**") # ATENÇÃO, todas requisições permitidas por padrão!
+        registry.addMapping("/**") // ATENÇÃO, todas requisições permitidas por padrão!
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
     }
 }
